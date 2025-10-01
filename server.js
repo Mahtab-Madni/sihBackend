6af7012151ab1126364e5095aeb0d82d4aba27ca
Mahtab Madni <mahtabjmi2005@gmail.com>
2025-10-01 19:10:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ const PORT = process.env.PORT || 5000;
 
 // ---------- MIDDLEWARE ----------
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:8080", // linking fortened server during development
+}));
 
 // ---------- MONGODB ----------
 mongoose
